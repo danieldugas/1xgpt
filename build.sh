97140b6dd729445816afe72e8e9b58f34d2add56
Daniel Dugas <daniel@dugas.ch>
2025-02-18 22:44:39 +0000

    logging steps

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
+set +x
+set +e
 
-python3 -m venv venv
-source venv/bin/activate
-python -m pip install -r requirements.txt
+conda env create -f environment.yaml
+conda activate genie
 FLASH_ATTENTION_SKIP_CUDA_BUILD=TRUE python -m pip install flash-attn==2.5.8 --no-build-isolation
 
+mkdir -p /checkpoint/cortex/dud/1xgpt_data
+ln -s /checkpoint/cortex/dud/1xgpt_data /home/dud/Code/1xgpt/data
 # Download datasets to data/train_v1.0, data/val_v1.0
 huggingface-cli download 1x-technologies/worldmodel --repo-type dataset --local-dir data
